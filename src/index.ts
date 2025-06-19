2b507ce06674c0b8387952bf48736952423b6a5b
Jinbeom Kim <jb.kim@eastarjet.com>
2025-06-20 08:31:29 +0900

    💚 Fix: 배포

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 export { cn } from './lib/utils';
 
 // UI Components
-export * from './ui/display';
-export * from './ui/input';
-export * from './ui/feedback';
-export * from './ui/navigation';
+export * as display from './ui/display';
+export * as input from './ui/input';
+export * as feedback from './ui/feedback';
+export * as navigation from './ui/navigation';
