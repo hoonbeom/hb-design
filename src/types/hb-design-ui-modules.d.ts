7a1df531ec9b144f320e188569211d63f762ea4d
Jinbeom Kim <jb.kim@eastarjet.com>
2025-06-20 14:32:21 +0900

    💚 Fix: 배포

---
+++ src/types/hb-design-ui-modules.d.ts
@@ -0,0 +1,15 @@
+declare module 'hb-design/ui/display' {
+  export * from '../ui/display';
+}
+
+declare module 'hb-design/ui/input' {
+  export * from '../ui/input';
+}
+
+declare module 'hb-design/ui/feedback' {
+  export * from '../ui/feedback';
+}
+
+declare module 'hb-design/ui/navigation' {
+  export * from '../ui/navigation';
+}
